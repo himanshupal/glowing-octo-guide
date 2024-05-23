ba468248b5e1d1f7b06de748e8c45ec3afd81bfe
Himanshu Pal <hpal@roambee.com>
2024-05-23 07:34:05 +0530

    Added working 'identify' endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 import { ZodError, z } from "zod";
-import { getNumberOrFallback } from "./utils";
+import { generatePayload, getNumberOrFallback } from "./utils";
 
 const server = express();
 
@@ -23,8 +23,65 @@ server.get("/", (_req, res) => res.sendStatus(200));
 
 server.get("/identify", async (req, res) => {
   try {
-    const data = contactBaseSchema.parse(req.body);
-    return res.json(data);
+    const { email, phoneNumber } = contactBaseSchema.parse(req.body);
+
+    const contacts = await prisma.contact.findMany({
+      where: {
+        OR: [{ email }, { phoneNumber }],
+      },
+    });
+
+    if (!contacts.length) {
+      // Create contact if not exists
+      const created = await prisma.contact.create({
+        data: {
+          linkPrecedence: "primary",
+          phoneNumber,
+          email,
+        },
+      });
+
+      const finalPayload = generatePayload(created.id, [created]);
+      return res.json({ contact: finalPayload });
+    }
+
+    {
+      // Check if list has some primary contact
+      const primaryContactFound = contacts.find((contact) => contact.linkPrecedence === "primary");
+      if (primaryContactFound) {
+        // We have found a primary contact, thus it will have all the secondary contacts in it's `linkedTo` part
+        const secondaryContacts = await prisma.contact.findMany({
+          where: {
+            AND: [
+              {
+                // Find secondary contacts linked to this primary contact
+                linkedId: primaryContactFound.id,
+              },
+              {
+                id: {
+                  // Do not fetch the already fetched contacts
+                  notIn: contacts.map(({ id }) => id),
+                },
+              },
+            ],
+          },
+        });
+
+        const finalPayload = generatePayload(primaryContactFound.id, [...contacts, ...secondaryContacts]);
+        return res.json({ contact: finalPayload });
+      }
+    }
+
+    // We have found a secondary contact with no primary contact, thus need to fetch the rest using its `linkedId`
+    const primaryContactId = contacts[0].linkedId!;
+    const secondaryContacts = await prisma.contact.findMany({
+      where: {
+        OR: [{ id: primaryContactId }, { AND: [{ linkedId: primaryContactId }, { linkedId: { notIn: contacts.map(({ id }) => id) } }] }],
+      },
+    });
+
+    const finalPayload = generatePayload(primaryContactId, [...contacts, ...secondaryContacts]);
+    return res.json({ contact: finalPayload });
   } catch (err) {
     if (err instanceof ZodError) {
       return res.status(400).json(err.errors);
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,20 @@
+import type { Contact } from "@prisma/client";
+import type { IIdentifyResponse } from "./types";
+
 export const getNumberOrFallback = (value: unknown, fallback = 0): number => {
   const parsed = Number(value);
   return Number.isNaN(parsed) ? fallback : parsed;
+};
+
+export const generatePayload = (primaryContactId: number, contacts: Contact[]): IIdentifyResponse["contact"] => {
+  const emails = [...new Set(contacts.map(({ email }) => email!))];
+  const phoneNumbers = [...new Set(contacts.map(({ phoneNumber }) => phoneNumber!))];
+  const secondaryContactIds = [...new Set(contacts.map(({ id }) => id).filter((id) => id !== primaryContactId))];
+
+  return {
+    primaryContactId,
+    secondaryContactIds,
+    phoneNumbers,
+    emails,
+  };
 };
